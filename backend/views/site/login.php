20fede9d04967c9ac8216da0095f8ff7e47fb7d0
henry <henrobats@gmail.com>
2018-05-25 11:07:02 +0100

    Backend Template
    Template integrated into backend and login.

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -1,35 +1,132 @@
 <?php
+  use yii\helpers\Html;
+  use yii\bootstrap\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \common\models\LoginForm */
 
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<!DOCTYPE html>
+<html lang="en">
 
-    <p>Please fill out the following fields to login:</p>
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+    <!-- Title Page-->
+    <title>Login</title>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+    <!-- Fontfaces CSS-->
+    <?= Html::cssFIle('@web/css/font-face.css'); ?>
+    <?= Html::cssFIle('@web/vendor/font-awesome-4.7/css/font-awesome.min.css'); ?>
+    <?= Html::cssFIle('@web/vendor/font-awesome-5/css/fontawesome-all.min.css'); ?>
+    <?= Html::cssFIle('@web/vendor/mdi-font/css/material-design-iconic-font.min.css'); ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+    <!-- Bootstrap CSS-->
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+    <?= Html::cssFIle('@web/vendor/bootstrap-4.1/bootstrap.min.css'); ?>
+    <!-- Vendor CSS-->
+
+    <?= Html::cssFIle('@web/vendor/animsition/animsition.min.css'); ?>
+    <?= Html::cssFIle('@web/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.cs'); ?>
+    <?= Html::cssFIle('@web/vendor/wow/animate.css'); ?>
+    <?= Html::cssFIle('@web/vendor/css-hamburgers/hamburgers.min.css'); ?>
+    <?= Html::cssFIle('@web/vendor/slick/slick.css'); ?>
+    <?= Html::cssFIle('@web/vendor/select2/select2.min.css'); ?>
+    <?= Html::cssFIle('@web/vendor/perfect-scrollbar/perfect-scrollbar.css'); ?>
+
+    <!-- Main CSS-->
+
+    <?= Html::cssFIle('@web/css/theme.css'); ?>
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+                        <div class="login-logo">
+                            <a href="#">
+                              <?= Html::img('@web/images/dcalogo.png', ['alt' => '', 'id' => '', 'class' => '']); ?>
+                            </a>
+                        </div>
+                        <div class="login-form">
+
+
+                            <?php $form = ActiveForm::begin(['id' => 'user-form']); ?>
+                                <div class="form-group">
+                                    <label>Email Address</label>
+                                    <?= $form->field($model, 'username')->textInput(['autofocus' => false, 'class'=>'au-input au-input--full'])
+                                    ->label(false);?>
+
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <?= $form->field($model, 'password')->passwordInput(['class'=>'au-input au-input--full'])->label(false); ?>
+
+                                </div>
+                                <div class="login-checkbox">
+                                    <label>
+                                        Remember Me
+                                        <?= $form->field($model, 'rememberMe')->checkbox() ->label(false); ?>
+
+                                    </label>
+                                    <label>
+                                        <a href="#">Forgotten Password?</a>
+                                    </label>
+                                </div>
+                                <?= Html::submitButton('Login', ['class' => 'au-btn au-btn--block au-btn--green m-b-20', 'name' => 'login-button']) ?>
+
+                                <div class="social-login-content">
+                                    <div class="social-button">
+                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button>
+
+                                    </div>
+                                </div>
+                            <?php ActiveForm::end(); ?>
+                            <div class="register-link">
+                                <p>
+                                    Don't you have account?
+                                    <a href="#">Sign Up Here</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
+
     </div>
-</div>
+
+    <!-- Jquery JS-->
+    <?= Html::jsFIle('@web/vendor/jquery-3.2.1.min.js'); ?>
+
+    <!-- Bootstrap JS-->
+    <?= Html::jsFIle('@web/vendor/bootstrap-4.1/popper.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/bootstrap-4.1/bootstrap.min.js'); ?>
+
+    <!-- Vendor JS       -->
+
+    <?= Html::jsFIle('@web/vendor/slick/slick.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/wow/wow.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/animsition/animsition.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/counter-up/jquery.waypoints.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/counter-up/jquery.counterup.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/circle-progress/circle-progress.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/perfect-scrollbar/perfect-scrollbar.js'); ?>
+    <?= Html::jsFIle('@web/vendor/chartjs/Chart.bundle.min.js'); ?>
+    <?= Html::jsFIle('@web/vendor/select2/select2.min.js'); ?>
+    <?= Html::jsFIle('@web/js/main.js'); ?>
+
+
+
+</body>
+
+</html>
+<!-- end document-->
