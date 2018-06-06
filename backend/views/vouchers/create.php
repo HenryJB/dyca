141175ea3f232a1a097769797a1a42fe153f88dd
henry <henrobats@gmail.com>
2018-06-06 18:37:31 +0100

    Vouchers Modules Added
    Vouchers Modules Added

---
+++ backend/views/vouchers/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Voucher */
+
+$this->title = 'Create Voucher';
+$this->params['breadcrumbs'][] = ['label' => 'Vouchers', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="voucher-create">
+
+    <h3><?= Html::encode($this->title) ?></h3>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
